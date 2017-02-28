6abf445c7ef74edeebdc2d04d7f171bef6ee2dad
Leonora Panagiotidou <leonora@naturalcycles.com>
2017-02-28 23:59:48 +0100

    Updates

--- bundle.js
+++ bundle.js
@@ -9875,7 +9875,7 @@ var CardData = {
     titles: ['Pink pork loved your tattoo!', 'Pink Royale added new art!', 'Pork Royale invites you to join their studio.'],
     descriptions: ['Etiam sit amet ex quis magna accumsan sollicitudin ac sit amet lacus. Nunc at ipsum augue.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent erat diam, luctus eget nunc eget, viverra luctus augue', 'Nunc et magna vitae enim faucibus dictum. Nulla at tellus turpis. Pellentesque ornare.'],
     usersNames: ['Pink Royale', 'Pork Royale', 'Pink Pork'],
-    userImages: ['http://s23.postimg.org/ssdslelvv/ib_pink_Royale.png', 'https://s14.postimg.org/7cy2i2vch/ib_pork_Royale.png'],
+    userImages: ['https://s23.postimg.org/ssdslelvv/ib_pink_Royale.png', 'https://s14.postimg.org/7cy2i2vch/ib_pork_Royale.png'],
     userTypes: ['artist', 'studio', 'monkey']
 };
 
